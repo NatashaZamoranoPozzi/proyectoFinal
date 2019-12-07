05d67b065eca13ffd4f6a264af1fecee6fd3cb73
NatashaZamoranoPozzi <natu.z@live.com>
2019-12-06 21:09:19 -0300

    algunas cositas

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -31,19 +33,16 @@
                                     <img class="img-fluid logo" src="https://instagram.faep9-1.fna.fbcdn.net/vp/2ae8249ed6e76a4df5b5498871024f5b/5E5C3D6D/t51.2885-19/s150x150/62624376_2233291810094827_6461921087314722816_n.jpg?_nc_ht=instagram.faep9-1.fna.fbcdn.net" alt="Logo Principial">
                             </div>
                         </div>
-                        
+                        <? if(!isset($_SESSION["emailUsuario"])){ ?>
                         <div class="row">
-                        <?// if(!isset($_SESSION["nombreUsuario"])){ ?>
                             <div class="col-md-6 text-center mt-3">
                                 <a class="btn bg-color1 text-white d-block" href="registro.php">Registro</a>
                             </div>
-                        <?//}?>
-                        <?// if(!isset($_SESSION["nombreUsuario"])){ ?>
                         <div class="col-md-6 text-center mt-3">
                                 <a class="btn bg-color1 text-white d-block" href="login.php">Login</a>
                             </div>
                         </div>
-                        <?//}?>
+                        <?}?>
                     </div>
                 </div>
             </div>
