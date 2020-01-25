f78a4d09372df5805130f9f614ff7394dc591bce
leoscabyx <ing.leonardo.lovera@gmail.com>
2020-01-25 15:16:56 -0300

    modificando partials, agreando include header-nav-footer

---
+++ partials/footer.php
@@ -0,0 +1,42 @@
+<footer class="container-fluid bg-dark py-4 text-white">
+        <section class="container">
+                <strong class="ff_titulo display-4">Cocolo-ve</strong>
+            <div class="row">
+                <div class="col-md-6">
+                
+                    <p>Hand Made Accessories <br> Hope you feel beautiful with #cocolo</p>
+                    
+                </div>
+                <div class="col-md-6">
+                    <p>Ciudad Autonoma de Buenos Aires, Argentina.</p>
+                    <ul>
+                        <li>Puntos de entrega en Caballito</li>
+                        <li>Envios a todo el país</li>
+                    </ul>
+                    <p>Seguinos en redes y WhatsApp!</p>
+                    <a href="https://instagram.com/cocolo_ve?igshid=67qwjzxr7sks"><ion-icon class="color1" name="logo-instagram" size="large"></ion-icon></i></a>
+                    <a href=""><ion-icon class="color1" name="logo-pinterest" size="large"></ion-icon></a>
+                    <a class="color1" href="tel:+5491123898777"><ion-icon class="color1" name="logo-whatsapp" size="large"></ion-icon></a> 
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 py-2">
+                    <p>Derechos Reservados - &copy; <?php  echo date('Y');  ?> </p>
+                </div>
+            </div>
+        </section>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+</body>
+
+</html>
--- partials/header.php
+++ partials/header.php
@@ -1,57 +1,15 @@
-<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="es">
 
-<header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-color1">
-            <div class="container">
-            <a class="navbar-brand ff_titulo" href="index.html">Cocolo-ve</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-    
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link text-white" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link text-white" href="productos.php">Productos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="faq.php">F.A.Q.</a>
-                    </li>
-                    <?php if (isset($_SESSION["nombreUsuario"])) { ?>
-                        <li class="nav-item">
-                        <a class="nav-link text-white" href="perfil.php">Perfil</a>
-                    <?php } ?>
-                    
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="contacto.php">Contacto</a>
-                    </li>
-                    <?php if(!isset($_SESSION["nombreUsuario"])){ ?>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="registro.php">Registro</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="login.php">Login</a>
-                    </li>
-                    <?php } ?>
-                    <li class="nav-item">
-                    <?php if(isset($_SESSION["nombreUsuario"])){ ?>
-                        <a class="nav-link text-white" href="partials/cerrar.php">Cerrar Sesión</a>
-                    </li>
-                    <?php } ?>
-                    
-                </ul>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-                <ul class="navbar-nav ml-auto">
-                <li><?php // echo $_SESSION["nombreUsuario"] ?></li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="carro-compra.php"><ion-icon name="cart" size=""></ion-icon></a>
-                    </li>
-                </ul>
-            </div>
-            </div>
-        </nav>
-    </header>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title><?= $title; ?></title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
